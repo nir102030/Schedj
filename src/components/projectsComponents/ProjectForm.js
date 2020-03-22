65e4a2466f6ad544b4cac9df0929179f359b140b
baryanuka <yanukab@post.bgu.ac.il>
2020-03-22 20:11:16 +0200

    Ahi halas

--- src/components/projectsComponents/ProjectForm.js
+++ src/components/projectsComponents/ProjectForm.js
@@ -9,15 +9,9 @@ import FormSubmitButton from '../genericComponents/FormSubmitButton';
 const ProjectForm = ({initialValues, onSubmit}) => {
     //TODO: 1.get the participants list from the reducers/index 
     const [name,setName] = useState(initialValues.name);
-<<<<<<< HEAD
-    const [minForMeeting, setMinForMeeting] = useState('');
-    const [participants,setParticipants] = useState(['Nir','Bar']);
     const [note,setNote]  = useState('');
-
-=======
     const [minForMeeting, setMinForMeeting] = useState(initialValues.minForMeeting);
     const [participants,setParticipants] = useState(initialValues.participants);
->>>>>>> 0e0b091282965b05333a274df17955deb14b378d
 
     return (
             <View style= {styles.container}>
@@ -25,14 +19,10 @@ const ProjectForm = ({initialValues, onSubmit}) => {
                 <FormInput title='Project Name' value = {name} onChange={setName} viewStyle = {styles.viewStyle}/>
                 <FormParticipantsList initialList = {participants}/>
                 <FormInput title='Min for Meeting' value={minForMeeting} onChange = {setMinForMeeting} viewStyle = {styles.viewStyle}/> 
-<<<<<<< HEAD
                 <FormInput title='1.' value = {note} onChange={setNote} viewStyle = {styles.viewStyle}/>
                 <FormInput title='2.' value = {note} onChange={setNote} viewStyle = {styles.viewStyle}/>
                 <FormInput title='3.' value = {note} onChange={setNote} viewStyle = {styles.viewStyle}/>
-                <FormSubmitButton onSubmit = {() => onSubmit(name)}/>
-=======
                 <FormSubmitButton onSubmit = {() => onSubmit(name,minForMeeting)}/>
->>>>>>> 0e0b091282965b05333a274df17955deb14b378d
             </View>
     );
 };
