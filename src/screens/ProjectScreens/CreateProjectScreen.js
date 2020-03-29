b40f0370d8cbba6ef7850529b8c7b6c3d0764e70
nir102030 <nir102030@gmail.com>
2020-03-29 11:02:08 +0300

    create project screen change

--- src/screens/ProjectScreens/CreateProjectScreen.js
+++ src/screens/ProjectScreens/CreateProjectScreen.js
@@ -5,11 +5,7 @@ import * as actions from '../../actions';
 import { connect } from 'react-redux';
 
 const CreateProjectScreen = ({navigation, addProject}) => {
-<<<<<<< HEAD
     const project = {id: '', name:'', minForMeeting: '', participants:[], reminder: '' ,notes: ['','','']}
-=======
-    const project = {id: '', name: '', minForMeeting: '', participants:[], reminder: '',notes: ['','','']}
->>>>>>> 03d9acbda043f4dacebf1a772b4dadd7d0300b3b
 
     return (
         <View>
